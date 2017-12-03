35c2892ae901b3a78f689f9edda76c8d31ed5227
rsuela <rsuela@gmail.com>
2017-12-04 00:17:26 +0800

    implemented code for sidepanel

--- serversurveillance/servermon/static/servermon/js/main.js
+++ serversurveillance/servermon/static/servermon/js/main.js
@@ -11,15 +11,47 @@
       $('#header').removeClass('scrolled')
     }
   })
-
 })(jQuery);
+
+function bindButtonClick(){
+    $('.card').click(function(){
+        var cardid;
+        cardid = $(this).find('span').attr('cardid');
+      
+        $.ajax({
+            url: "/server_details?server_id=" + cardid,
+            type: "GET",
+            card_id: cardid,
+            success: function(data, textStatus) {
+                $('.container-sidepanel').html(data);
+            }
+        });
+      $('.container-sidepanel').toggleClass('open');
+    });
+        
+    $(document).mouseup(function(e) 
+    {
+        var container = $('.container-sidepanel');
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {
+            $('.container-sidepanel').removeClass('open');
+        }
+    });
+}
+
+
 
 $('document').ready(function(){
     $('#search,#searchinput').on('keyup click', function(e){
         var search_term = $('#searchinput').val();
         $.get( "search?q=" + search_term, function( data ) {
             $('.card-container').html(data);
-            console.log(data);
+            bindButtonClick();
         });
+        
     });
+    
+    bindButtonClick();
+    
+
 });
