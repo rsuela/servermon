3c5744b082c59961cacbdd74d9d8a7714de26edc
Rey Angelo Suela <rs185275@ncr.com>
2017-12-08 20:21:30 +0800

    changes on UI side panel

--- serversurveillance/client.py
+++ serversurveillance/client.py
@@ -7,6 +7,7 @@ import psutil
 import uuid
 from subprocess import Popen, PIPE, STDOUT
 import re
+import sys
 # from _winreg import *
 from winreg import *
 
@@ -46,8 +47,11 @@ def on_open(ws):
             perm = (KEY_WOW64_64KEY | KEY_ALL_ACCESS) if permFlag else KEY_ALL_ACCESS
             reg_key = OpenKey(HKEY_LOCAL_MACHINE, "%s"%(reg_path), 0, perm)
             result = QueryValueEx(reg_key, target)
-        except:
-            print('target not found: %s'%target)
+        except PermissionError:
+            print('WINDOWS ERROR: No permission to access registry')
+            pass
+        except FileNotFoundError:
+            print('The target %s was not found in the registry'%(reg_path + "::" + target))
             pass
         return result
 
@@ -56,16 +60,59 @@ def on_open(ws):
         EPM = get_registry_value(r'SOFTWARE\WOW6432Node\NCR\PreferenceManager', 'ProductVersion', False)
         Broker = get_registry_value(r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AMS Brokers', 'DisplayVersion', False)
         UE = get_registry_value(r'SOFTWARE\NCR\AMS', 'ProductVersion', False)
-
-        all_products = "'logix':'%s','epm':'%s','broker':'%s','ue':'%s'" %(logix[0],EPM[0],Broker[0],UE[0])
-        return all_products
+        
+        all_products = []
+        
+        if logix:
+            all_products.append("'logix':'%s'"%(logix[0]))
+            
+        if EPM:
+            all_products.append("'epm':'%s'"%(EPM[0]))
+        
+        if Broker:
+            all_products.append("'broker':'%s'"%(Broker[0]))
+            
+        if UE:
+            all_products.append("'ue':'%s'"%(UE[0]))
+        
+        found = ""
+        for product in all_products:
+            found = found + product + ","
+        
+        # all_products = "%s,%s,%s,%s" %(logix[0],EPM[0],Broker[0],UE[0])
+        if found:
+            found = found[0:len(found)-1]
+        print(found)
+        return found
     
 ############# END SOFTWARE LIST ################
 
     def get_software_list():
-        get_ams_product()
-        software_list = "{'openssl':'%s','java':'%s',%s}"%(get_openssl_version(),get_java_version(),get_ams_product())
-        return software_list
+        software_list = []
+        
+        openssl = get_openssl_version()
+        ams = get_ams_product()
+        java = get_java_version()
+        
+        if openssl:
+            software_list.append("'openssl':'%s'"%(openssl))
+        
+        if java:
+            software_list.append("'java':'%s'"%(java))
+            
+        if ams:
+            software_list.append(get_ams_product())
+            
+        found = "{"
+        for software in software_list:
+            found = found + software + ","
+            
+        if found:
+            found = found[0:len(found)-1] + "}"
+        
+        print(found)
+        # software_list = "{'openssl':'%s','java':'%s',%s}"%(get_openssl_version(),get_java_version(),get_ams_product())
+        return found
 
     def run(*args):
         linux_sys = "'%s %s'}"%(platform.linux_distribution()[0],platform.linux_distribution()[1])
@@ -87,8 +134,16 @@ def on_open(ws):
 
 
 if __name__ == "__main__":
+    
+    if len(sys.argv) < 2:
+        print("Error please specify the servermon master hostname")
+        sys.exit(1)
+    #endif
+    
+    servermon = sys.argv[1]
+    
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://127.0.0.1:8000/connect",
+    ws = websocket.WebSocketApp("ws://%s/connect"%(servermon),
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
